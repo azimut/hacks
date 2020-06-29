2a36b705026230ad95bcd3473317fc231488874d
azimut <azimut.github@protonmail.com>
2020-06-29 13:29:45 -0300

    add some fields to table

--- orejon/main.py
+++ orejon/main.py
@@ -5,14 +5,24 @@ from mitmproxy import http
 from mitmproxy import addonmanager
 from mitmproxy.script import concurrent
 
+import pprint
 import psycopg2
 
 TABLE_DEFINITION = """
 DROP TABLE http_entries;
 CREATE TABLE IF NOT EXISTS http_entries(
-    path VARCHAR(128)
+    timestamp TIMESTAMP DEFAULT NOW(),
+    method    VARCHAR(8),
+    scheme    VARCHAR(8),
+    host      VARCHAR(256),
+    port      SMALLINT,
+    path      VARCHAR(256),
+    version   VARCHAR(16),
+    status    SMALLINT
 );
 """
+
+pp = pprint.PrettyPrinter(indent=4)
 
 try:
     conn = psycopg2.connect("dbname=postgres user=postgres")
@@ -30,27 +40,34 @@ def done():
 
 @concurrent
 def response(flow: http.HTTPFlow) -> None:
-     data = {
-         'request:url': flow.request.url,
-         'request:method': flow.request.method,
-         'request:scheme': flow.request.scheme,
-         'request:http_version': flow.request.http_version,
-         'request:host': flow.request.host,
-         'request:port': flow.request.port,
-         'request:path': flow.request.path,
-         'request:headers': flow.request.headers,
-         #        'request:content': flow.request.get_content(),
-         'request:timestamp_start': flow.request.timestamp_start,
-         'request:timestamp_end': flow.request.timestamp_end,
-         'response:http_version': flow.response.http_version,
-         'response:status_code': flow.response.status_code,
-         'response:headers': flow.response.headers,
-         #        'response:content': flow.response.get_content(),
-         'response:timestamp_start': flow.response.timestamp_start,
-         'response:timestamp_end': flow.response.timestamp_end,
-     }
-     with conn:
-         with conn.cursor() as cur:
-             cur.execute("INSERT INTO http_entries VALUES (%s)",
-                         (flow.request.host + flow.request.path,))
-     print(data)
+    data = {
+        'request:url': flow.request.url,
+        'request:method': flow.request.method,
+        'request:scheme': flow.request.scheme,
+        # 'request:http_version': flow.request.http_version,
+        'request:host': flow.request.host,
+        'request:port': flow.request.port,
+        'request:path': flow.request.path,
+        # 'request:headers': flow.request.headers,
+        # 'request:content': flow.request.get_content(),
+        # 'request:timestamp_start': flow.request.timestamp_start,
+        # 'request:timestamp_end': flow.request.timestamp_end,
+        'response:http_version': flow.response.http_version,
+        'response:status_code': flow.response.status_code,
+        'response:headers': flow.response.headers,
+        # 'response:content': flow.response.get_content(),
+        # 'response:timestamp_start': flow.response.timestamp_start,
+        # 'response:timestamp_end': flow.response.timestamp_end,
+    }
+    with conn:
+        with conn.cursor() as cur:
+            cur.execute("""INSERT INTO http_entries(port, scheme, host, version, status, path, method)
+                           VALUES (%s,%s,%s,%s,%s,%s,%s)""",
+                        (flow.request.port,
+                         flow.request.scheme,
+                         flow.request.host,
+                         flow.response.http_version,
+                         flow.response.status_code,
+                         flow.request.path,
+                         flow.request.method,))
+    pp.pprint(data)
