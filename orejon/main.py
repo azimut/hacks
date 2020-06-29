b7accbf01d716effcff81ce5123d3a679d402f2d
azimut <azimut.github@protonmail.com>
2020-06-29 18:18:19 -0300

    cleanup

--- orejon/main.py
+++ orejon/main.py
@@ -51,25 +51,6 @@ def done():
 
 @concurrent
 def response(flow: http.HTTPFlow) -> None:
-    data = {
-        'request:url': flow.request.url,
-        'request:method': flow.request.method,
-        'request:scheme': flow.request.scheme,
-        # 'request:http_version': flow.request.http_version,
-        'request:host': flow.request.host,
-        'request:port': flow.request.port,
-        'request:path': flow.request.path,
-        # 'request:headers': flow.request.headers,
-        # 'request:content': flow.request.get_content(),
-        # 'request:timestamp_start': flow.request.timestamp_start,
-        # 'request:timestamp_end': flow.request.timestamp_end,
-        'response:http_version': flow.response.http_version,
-        'response:status_code': flow.response.status_code,
-        'response:headers': flow.response.headers,
-        # 'response:content': flow.response.get_content(),
-        # 'response:timestamp_start': flow.response.timestamp_start,
-        # 'response:timestamp_end': flow.response.timestamp_end,
-    }
     with conn:
         with conn.cursor() as cur:
             cur.execute("""INSERT INTO http_entries(port, scheme, host, version, status, path, method, length, headers)
